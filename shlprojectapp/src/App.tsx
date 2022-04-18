ecbf5d24cdbc84873b6d0b8adad10a82ed7a682a
davism2 <64663157+davism2@users.noreply.github.com>
2022-04-18 19:33:48 -0400

    Logo, basic sidebar

--- shlprojectapp/src/App.tsx
+++ shlprojectapp/src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./shlLogo.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <p> Hello World!</p>
-  )
+    <>
+      <div className="wrapper">
+        <div className="sidebar">
+          <img src={logo} className="logo" alt="Test logo" />
+          <h3> Login</h3>
+          <h3> Top 10</h3>
+          <h3> Player Graphing</h3>
+          <h3> Team Graphing</h3>
+          <h3> Player Statistics</h3>
+          <h3> Team Statistics</h3>
+          <h3> Ideal Build</h3>
+          <h3> Profile</h3>
+        </div>
+      </div>
+      <body>Hello World!</body>
+    </>
+  );
 }
 
-export default App
+export default App;
