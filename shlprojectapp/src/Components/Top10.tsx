c712571465013f41e3d08dffa96ea1bf8412f9ca
davism2 <64663157+davism2@users.noreply.github.com>
2022-04-19 19:06:23 -0400

    Get Skater top10 data form API

--- shlprojectapp/src/Components/Top10.tsx
+++ shlprojectapp/src/Components/Top10.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import "../App.css";
+import axios, { AxiosResponse } from "axios";
+import { top10Skater } from "../datatypes";
 
 function Top10() {
+  useEffect(() => {
+    console.log("Mounting Dom!");
+    getGoals();
+  });
+
+  const getGoals = () => {
+    axios
+      .request({
+        method: "GET",
+        url: "https://index.simulationhockey.com/api/v1/leaders/skaters/goals",
+      })
+      .then((r: AxiosResponse) => r.data)
+      .then((r: top10Skater) => {
+        console.log(r);
+      });
+  };
+
   return (
     <>
       <h1>Hello!</h1>
