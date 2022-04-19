81a52952a314c166e775cb7c26078df44107023d
davism2 <64663157+davism2@users.noreply.github.com>
2022-04-18 20:04:00 -0400

    Basic sidebar

--- shlprojectapp/src/App.tsx
+++ shlprojectapp/src/App.tsx
@@ -1,26 +1,16 @@
 import { useState } from "react";
 import logo from "./shlLogo.png";
 import "./App.css";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="wrapper">
-        <div className="sidebar">
-          <img src={logo} className="logo" alt="Test logo" />
-          <h3> Login</h3>
-          <h3> Top 10</h3>
-          <h3> Player Graphing</h3>
-          <h3> Team Graphing</h3>
-          <h3> Player Statistics</h3>
-          <h3> Team Statistics</h3>
-          <h3> Ideal Build</h3>
-          <h3> Profile</h3>
-        </div>
+      <div className="App">
+        <Sidebar />
       </div>
-      <body>Hello World!</body>
     </>
   );
 }
--- shlprojectapp/src/components/Sidebar.tsx
+++ shlprojectapp/src/components/Sidebar.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "../App.css";
 
 function Sidebar() {
-  return <></>;
+  return (
+    <>
+      <div className="sidebar">
+        {/* <img src={logo} className="logo" alt="Test logo" /> */}
+        {/* <h3> Login</h3>
+        <h3> Top 10</h3>
+        <h3> Player Graphing</h3>
+        <h3> Team Graphing</h3>
+        <h3> Player Statistics</h3>
+        <h3> Team Statistics</h3>
+        <h3> Ideal Build</h3> */}
+      </div>
+    </>
+  );
 }
 
 export default Sidebar;
