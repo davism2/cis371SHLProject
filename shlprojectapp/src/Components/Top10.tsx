5d0ea541e1b65468865f81fc08cd7db150f17e8e
davism2 <64663157+davism2@users.noreply.github.com>
2022-04-19 20:13:04 -0400

    Attempt at table

--- shlprojectapp/src/Components/Top10.tsx
+++ shlprojectapp/src/Components/Top10.tsx
@@ -5,6 +5,8 @@ import axios, { AxiosResponse } from "axios";
 import { top10Skater } from "../datatypes";
 
 function Top10() {
+  const [goals, setGoals] = useState({} as top10Skater[]);
+
   useEffect(() => {
     console.log("Mounting Dom!");
     getGoals();
@@ -17,13 +19,29 @@ function Top10() {
         url: "https://index.simulationhockey.com/api/v1/leaders/skaters/goals",
       })
       .then((r: AxiosResponse) => r.data)
-      .then((r: top10Skater) => {
+      .then((r: top10Skater[]) => {
         console.log(r);
+        // setGoals(r);
+        // console.log(goals);
       });
   };
 
   return (
     <>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Team</th>
+          <th>Goals</th>
+        </tr>
+        {/* {goals.map((val, pos) => (
+          <tr key={pos}>
+            <td>{val.name}</td>
+            <td>{val.team.abbr}</td>
+            <td>{val.stat}</td>
+          </tr>
+        ))} */}
+      </table>
       <h1>Hello!</h1>
     </>
   );
