b6f6eca5a9a4875ce547023272e8768fc7af51d6
Ayden <martiayd@mail.gvsu.edu>
2022-04-26 17:14:18 -0400

    updating from main

--- shlprojectapp/src/components/Top10.tsx
+++ shlprojectapp/src/components/Top10.tsx
@@ -6,10 +6,12 @@ import { top10Skater } from "../datatypes";
 
 function Top10() {
   const [goals, setGoals] = useState([] as top10Skater[]);
+  const [assists, setAssists] = useState([] as top10Skater[]);
 
   useEffect(() => {
     console.log("Mounting Dom!");
     getGoals();
+    getAssists();
   }, []);
 
   const getGoals = () => {
@@ -21,6 +23,18 @@ function Top10() {
       .then((r: AxiosResponse) => r.data)
       .then((r: top10Skater[]) => {
         setGoals(r);
+      });
+  };
+
+  const getAssists = () => {
+    axios
+      .request({
+        method: "GET",
+        url: "https://index.simulationhockey.com/api/v1/leaders/skaters/assists",
+      })
+      .then((r: AxiosResponse) => r.data)
+      .then((r: top10Skater[]) => {
+        setAssists(r);
       });
   };
 
@@ -40,7 +54,21 @@ function Top10() {
           </tr>
         ))}
       </table>
-      <h1>Hello!</h1>
+
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Team</th>
+          <th>Assists</th>
+        </tr>
+        {assists.map((val, pos) => (
+          <tr key={pos}>
+            <td>{val.name}</td>
+            <td>{val.team.abbr}</td>
+            <td>{val.stat}</td>
+          </tr>
+        ))}
+      </table>
     </>
   );
 }
